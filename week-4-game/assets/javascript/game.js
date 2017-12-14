bf926e6f848d83983809850a9f7fad39b305fc6f
sakiohma <cowilliams1989@yahoo.com>
2017-12-14 16:45:34 -0500

    Add files via upload

--- week-4-game/assets/javascript/game.js
+++ week-4-game/assets/javascript/game.js
@@ -1,99 +1,90 @@
-var winCounter = 0;
-var lossCounter = 0;
-var targetNumber = Math.floor(Math.random() * 101) + 19;
-$("#targetNumber").text(targetNumber);
+
+var random_result;
+var lose = 0;
+var win = 0;
+var previous = 0;
 
 
 
-var Cry1 = Math.floor(Math.random() * 11) + 1;
-var Cry2 = Math.floor(Math.random() * 11) + 1;
-var Cry3 = Math.floor(Math.random() * 11) + 1;
-var Cry4 = Math.floor(Math.random() * 11) + 1;
+var resetAndStart = function () {
+
+  $(".kitties").empty();
+
+  var images = ['yellowkitty.png', 
+                'sadkitty.jpg', 
+                'nomnomkitty.jpg', 
+                'bunnykitty.jpg'];
+
+  random_result = Math.floor(Math.random() * 69 ) + 30;
+
+$("#GoalNumber").html(random_result);
+
+for(var i = 0; i < 4; i++){
+
+  var random = Math.floor(Math.random() * 11) + 1;
 
 
-$("#winCounter").html(winCounter);
-$("#lossCounter").html(lossCounter);
 
-var counter = 0;
-$("#curScore").text(counter);
+  var kitty = $("<div>");
+    kitty.attr({
+      "class": 'kitty',
+      "data-random": random
 
-function restartGame() {
+    });
 
-targetNumber = Math.floor(Math.random() * 101) + 19;
-$("#targetNumber").text(targetNumber);
-Cry1 = Math.floor(Math.random() * 11) + 1;
-Cry2 = Math.floor(Math.random() * 11) + 1;
-Cry3 = Math.floor(Math.random() * 11) + 1;
-Cry4 = Math.floor(Math.random() * 11) + 1;
-counter = 0;
-$("#curScore").text(counter);
+    kitty.css({
+      "background-image":"url('" + images[i] + "')"
+      "background-size":"cover"
+    });
+
+
+
+  $(".kitties").append(kitty);
+
+
+    }
+
+     $("previous").html("Your total score: " + previous);
 
 }
 
-function win() {
+resetAndStart();
 
-	alert("You win!");
-	winCounter ++;
-	$("#winCounter").text(winCounter);
-	restartGame();
-}
 
-function lose() {
 
-	alert("You lose!");
-	lossCounter ++;
-	$("#lossCounter").text(lossCounter);
-	restartGame();
-}
+$(".kitty").on('click', function () {
 
-$('#Cry1').on ('click', function(){
-    counter = counter + Cry1;
-    
-    $('#curScore').text(counter); 
-          
-        if (counter == targetNumber){
-          win();
-        }
-        else if (counter > targetNumber){
-          lose();
-        }   
-  });  
 
-$('#Cry2').on ('click', function(){
-    counter = counter + Cry2;
-    
-    $('#curScore').text(counter); 
-         
-        if (counter == targetNumber){
-          win();
-        }
-        else if (counter > targetNumber){
-          lose();
-        }   
-  });  
+  var num = parseInt($(this).attr('data-random'));
 
-$('#Cry3').on ('click', function(){
-    counter = counter + Cry3;
-    
-    $('#curScore').text(counter); 
-          
-        if (counter == targetNumber){
-          win();
-        }
-        else if (counter > targetNumber){
-          lose();
-        }   
-  });  
+  previous += num;
 
-$('#Cry4').on ('click', function(){
-    counter = counter + Cry4;
-    
-    $('#curScore').text(counter); 
-         
-        if (counter == targetNumber){
-          win();
-        }
-        else if (counter > targetNumber){
-          lose();
-        }   
-  });
+  $("previous").html(previous);
+
+  console.log(previous);
+
+  if(previous > random_result){
+
+    lose++;
+
+    $("#lose").html("Losses: " + lose);
+
+    previous = 0;
+
+    resetAndStart();
+
+  }
+
+  else if(previous === random_result){
+
+    win++;
+
+    $("#win").html("Wins: " + win);
+
+    previous = 0;
+
+    resetAndStart();
+  }
+
+
+});
